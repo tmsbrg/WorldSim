aed1c51138d3314c07a8fca78cb6b30097769055
Thomas van der Berg <ik@thomasvanderberg.nl>
2016-07-31 15:03:14 +0200

    Add timeline slider

--- WorldSim/src/GUI.java
+++ WorldSim/src/GUI.java
@@ -1,22 +1,28 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
-public class GUI implements Runnable,ActionListener {
+public class GUI implements Runnable,ActionListener,ChangeListener {
 
 	public void run() {
         JFrame f = new JFrame("WorldSim");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(new FlowLayout());
 
+        JSlider timeline = new JSlider(JSlider.HORIZONTAL, 0, 400, 0);
+        timeline.addChangeListener(this);
+        timeline.setMajorTickSpacing(100);
+        timeline.setPaintTicks(true);
+        f.add(timeline);
+
+        /*
         f.add(new JLabel("Hello, world!"));
         JButton b = new JButton("Press me!");
         b.setActionCommand("wow");
         b.addActionListener(this);
         f.add(b);
+        */
 
         f.pack();
         f.setVisible(true);
@@ -30,6 +36,13 @@ public class GUI implements Runnable,ActionListener {
                 break;
             default:
                 break;
+        }
+    }
+
+    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider)e.getSource();
+        if (!source.getValueIsAdjusting()) {
+                System.out.println("new value: " + source.getValue());
         }
     }
 }
