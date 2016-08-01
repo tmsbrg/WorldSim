d0a32a938593b28005f2ec9c68e8a2de63ffe437
Thomas van der Berg <ik@thomasvanderberg.nl>
2016-08-01 16:51:13 +0200

    Create UI from mockup

--- WorldSim/src/GUI.java
+++ WorldSim/src/GUI.java
@@ -7,23 +7,48 @@ public class GUI implements Runnable,ActionListener,ChangeListener {
 
 	public void run() {
         JFrame f = new JFrame("WorldSim");
+
+        f.setPreferredSize(new Dimension(800, 400));
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(new FlowLayout());
+        
+        ImageIcon icon = new ImageIcon("data/map.png", "Map mockup");
+        JLabel mapLabel = new JLabel(icon);
+        f.add(mapLabel);
 
+        JTextArea infoArea = new JTextArea(
+                "Halverton - Castle\n"+
+                "Owned by Kingdom of Goldrock\n"+
+                "Controlled by Lord Averick of Halverton\n"+
+                "\n"+
+                "Population: ~50'000\n"+
+                "Garrison: 2'180\n"+
+                "\n"+
+                "Founded on 5 jan 98 by Kingdom of Dalmycia\n"
+        );
+        infoArea.setEditable(false);
+        infoArea.setFont(new Font("Serif", Font.PLAIN, 12));
+        infoArea.setLineWrap(true);
+        infoArea.setWrapStyleWord(true);
+        JScrollPane areaScrollPane = new JScrollPane(infoArea);
+        areaScrollPane.setVerticalScrollBarPolicy(
+                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        areaScrollPane.setPreferredSize(new Dimension(350, 250));
+        areaScrollPane.setBorder(
+            BorderFactory.createCompoundBorder(
+                BorderFactory.createCompoundBorder(
+                                BorderFactory.createTitledBorder("Selected tile"),
+                                BorderFactory.createEmptyBorder(5,5,5,5)),
+                areaScrollPane.getBorder()));
+        f.add(areaScrollPane);
+        
         JSlider timeline = new JSlider(JSlider.HORIZONTAL, 0, 400, 0);
         timeline.addChangeListener(this);
         timeline.setMajorTickSpacing(100);
         timeline.setPaintTicks(true);
+        timeline.setPreferredSize(new Dimension(700, 50));
         f.add(timeline);
-
-        /*
-        f.add(new JLabel("Hello, world!"));
-        JButton b = new JButton("Press me!");
-        b.setActionCommand("wow");
-        b.addActionListener(this);
-        f.add(b);
-        */
-
+        
         f.pack();
         f.setVisible(true);
 
